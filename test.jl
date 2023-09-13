574a14a4c7c5dcce12f202fc2afcc2e4fbf0c47e
小雨 <84885148+tangxiangong@users.noreply.github.com>
2023-09-13 17:03:33 +0800

    add Poisson 求解器

--- test.jl
+++ test.jl
@@ -1,1 +1,27 @@
-using LinearAlgerba
+# using Gridap
+
+# domain = (0.0, 1.0, 0.0, 1.0)
+# partition = (10, 10)
+# model = CartesianDiscreteModel(domain, partition)
+
+# writevtk(model, "test")
+
+# using Gridap, GridapMakie, GLMakie, GridapGmsh
+# # using FileIO    
+
+# # # mkdir("models")
+# # # mkdir("images")
+
+# # domain = (1,2,0,1)
+# # partition = (10,10)
+
+# model = GmshDiscreteModel("square_regular.msh") |> simplexify
+# # model = CartesianDiscreteModel(domain,partition) |> simplexify
+# Ωₕ = Triangulation(model)
+# writevtk(model, "models/model")
+
+# fig = plot(Ωₕ)
+# wireframe!(Ωₕ, color=:black, linewidth=2) 
+# scatter!(Ωₕ, marker=:star8, markersize=20, color=:blue)
+# save("images/model.png", fig)
+
