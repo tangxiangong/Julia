4b730deeb4c2c330a79d9c0b3e9bf5b65dff29c7
小雨 <84885148+tangxiangong@users.noreply.github.com>
2023-09-15 15:48:47 +0800

    .

--- test.jl
+++ test.jl
@@ -1,27 +1,16 @@
-# using Gridap
+function f(x::Vector{Float64})
+    a = Float64[]
+    for i in x
+        push!(a, i)
+    end
+    return nothing 
+end
 
-# domain = (0.0, 1.0, 0.0, 1.0)
-# partition = (10, 10)
-# model = CartesianDiscreteModel(domain, partition)
-
-# writevtk(model, "test")
-
-# using Gridap, GridapMakie, GLMakie, GridapGmsh
-# # using FileIO    
-
-# # # mkdir("models")
-# # # mkdir("images")
-
-# # domain = (1,2,0,1)
-# # partition = (10,10)
-
-# model = GmshDiscreteModel("square_regular.msh") |> simplexify
-# # model = CartesianDiscreteModel(domain,partition) |> simplexify
-# Ωₕ = Triangulation(model)
-# writevtk(model, "models/model")
-
-# fig = plot(Ωₕ)
-# wireframe!(Ωₕ, color=:black, linewidth=2) 
-# scatter!(Ωₕ, marker=:star8, markersize=20, color=:blue)
-# save("images/model.png", fig)
-
+function g(x::Vector{Float64})
+    a = Float64[]
+    sizehint!(a, 1000000)
+    for i in x
+        push!(a, i)
+    end
+    return nothing
+end
