c40730d329733201ecf442cd80df5bdd9f2cfef6
小雨 <84885148+tangxiangong@users.noreply.github.com>
2023-11-11 12:37:42 +0800

    update

--- NSDiffusion/NSDiffusion.jl
+++ NSDiffusion/NSDiffusion.jl
@@ -157,6 +157,8 @@ norm_error(̄u₁, u₂, dΩ)
 ```
 """
 function norm_error(u₁, u₂, dΩ, scarlar_or_vector="scalar")
+    @assert scarlar_or_vector in ["scalar", "vector"]
+    
     e = u₁ - u₂
     if scarlar_or_vector == "scalar"
         L²_error = ∫(e * e)dΩ |> sum |> sqrt
